85e23bb7870c694d6f40512119a29bcc188ff104
KynaChan <chau.t.chan@students.plymouth.ac.uk>
2025-04-15 22:30:06 +0100

    test

--- network_anomaly_detector.py
+++ network_anomaly_detector.py
@@ -1,33 +1,29 @@
-
 import os
 import subprocess
 
+import cicflowmeter
+import joblib
+
+
 import numpy as np
 import pandas as pd
-import joblib
-import cicflowmeter
-
 from sklearn.ensemble import IsolationForest, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 
 class AnomalyDetector:
     # Capture network traffic using tcpdump.
-    def capture_traffic(self, interface='eth0', count=100, output='capture.pcap'):
+    def capture_traffic(self, interface="eth0", count=100, output="capture.pcap"):
         command = f"sudo tcpdump -i {interface} -w {output} -c {count}"
         subprocess.run(command, shell=True)
         print(f"[SUCCESS] Traffic captured and saved to {output}\n")
 
     # Transform the captured data with CICFlowMeter.
-    def extract_features(self, input_file='capture.pcap', output_file='transformed_data.csv'):
-
-
+    def extract_features(
+        self, input_file="capture.pcap", output_file="transformed_data.csv"
+    ):
         print(f"[SUCCESS] Data transformed and saved to {output_file}\n")
-
 
     def preprocess_data(self, file_path):
         print("Data preprocessed")
         return df
-    
-
-
