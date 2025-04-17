fafea5537e55366cc40a7621f2dd2652c7697e32
kyna <92019943+KynaChan@users.noreply.github.com>
2025-04-17 21:40:46 +0100

    Add notes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 
 if __name__ == "__main__":
-    print("Hello, World!")
-    print("Hello, World!")
+    print("\n      ~ Halo im a smol tool ~  \n")
+    print("\n  ~ You are in: running main.py ~  \n")
+
+    print("\n  [QUESTION] Would you like TRAINER MODE or DETECTOR MODE? :)\n")
 
