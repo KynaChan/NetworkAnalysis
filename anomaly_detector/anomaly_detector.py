3876528ea8ad59572c6c249cb60542f735607f8c
kyna <92019943+KynaChan@users.noreply.github.com>
2025-04-19 18:19:12 +0100

    Rename var and add run function

--- anomaly_detector/anomaly_detector.py
+++ anomaly_detector/anomaly_detector.py
@@ -9,34 +9,52 @@ from sklearn.ensemble import IsolationForest
 
 class AnomalyDetector:
 
-    def __init__(self, model, processed_data):
+    def __init__(self, model, prep_data):
         self.if_model = model
-        self.processed_data = processed_data
+        self.prep_data = prep_data
 
         self.if_predictions = None
         self.anomalies = None
         self.benign = None
 
 
+# Main function to identify anomalies
+    def run(self):
+        try:
+            self.identify_anomalies()
+            return self.transform_predictions()
+            
+        except Exception as e:
+            raise Exception(f"\n  [ERROR] An error occurred: {e}\n")
+
+
+
+
     def identify_anomalies(self):
-        if self.if_model is None or self.processed_data is None:
+        if self.if_model is None or self.prep_data is None:
             raise ValueError("\n  [ERROR] Model or processed data not provided.\n")
 
         # Predict anomalies using the Isolation Forest model
-        self.if_predictions = self.if_model.predict(self.processed_data)
+        self.if_predictions = self.if_model.predict(self.prep_data)
 
-        # Separate anomalies and benign instances
-        self.anomalies = self.processed_data[self.if_predictions == -1]
-        self.benign = self.processed_data[self.if_predictions == 1]
+        ##?
+        self.anomalies = self.prep_data[self.if_predictions == -1] # do i need this?
+        self.benign = self.prep_data[self.if_predictions == 1]
 
         print(f"\n  [SUCCESS] Anomalies identified: {len(self.anomalies)}\n")
         print(f"\n  [SUCCESS] Benign instances identified: {len(self.benign)}\n")
-        return self.anomalies # do i return the benign instances too?
 
-    def save_anomalies(self, output_file="anomalies.csv"):
+    def transform_predictions(self):
+        if self.if_predictions is None:
+            raise ValueError("\n  [ERROR] No predictions to transform.\n")
+        
+        if_predictions = pd.Series(self.if_predictions).map({1: 0, -1: 1})
+        return if_predictions
+    
+
+# do i need this?
+    def extract_anomalies(self):
         if self.anomalies is None:
             raise ValueError("\n  [ERROR] No anomalies to save.\n")
 
-        # Save anomalies to a CSV file
-        self.anomalies.to_csv(output_file, index=False)
-        print(f"\n  [SUCCESS] Anomalies saved to {output_file}\n")
+        return self.anomalies
