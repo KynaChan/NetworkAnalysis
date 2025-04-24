ba825dc6b3db7d527665a6291c480941bc0e2281
kyna <92019943+KynaChan@users.noreply.github.com>
2025-04-24 18:46:44 +0100

    Split trainer into two classes for IF and RF

--- network_analyser/model_trainer.py
+++ network_analyser/model_trainer.py
@@ -1,6 +1,5 @@
 
 import pandas as pd
-import RfDataParams
 
 
 from sklearn.ensemble import IsolationForest, RandomForestClassifier
@@ -9,38 +8,45 @@ from sklearn.ensemble import IsolationForest, RandomForestClassifier
 class IfModelTrainer:
 
     def __init__(self, if_prep_data):
-        self.df = preprocessed_data
-        self.labels = labels
-
+        self.if_prep_data = if_prep_data
+        # self.labels = labels
 
     def train_if_model(self, n_estimators=None, contamination=None):
         return IsolationForest(
             n_estimators=n_estimators, contamination=contamination
-        ).fit(self.df)
+        ).fit(self.if_prep_data)
 
 
 
 class RfModelTrainer:
 
-    def __init__(self, rf_prep_data: RfDataParams):
-        self.rf_prep_data = rf_prep_data
+    def __init__(self, x_train,x_test, y_train, y_test):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+        self.rf_model = None
 
     def train_model(self, n_estimators=None):
         # Train Random Forest model
 
-        rf_model = RandomForestClassifier(n_estimators=n_estimators)
-        rf_model.fit(self.df, self.labels)
+        self.rf_model = RandomForestClassifier(n_estimators=n_estimators)
+        self.rf_model.fit(self.x_train, self.y_train, shuffle=True)
 
-        return rf_model
+        return self.rf_model
 
-
-    def test_model(self, rf_model, x_test, y_test):
+    def test_model(self):
         # Predict on the test set
-        y_pred = rf_model.predict(x_test)
+        y_pred = self.rf_model.predict(self.x_test)
 
         # Calculate accuracy
-        accuracy = rf_model.accuracy_score(y_test, y_pred)
+        accuracy = self.rf_model.accuracy_score(self.y_test, y_pred)
         print(f"\n  [SUCCESS] Random Forest model trained with accuracy: {accuracy:.2f}\n")
-        print(rf_model.classification_report(y_test, y_pred))
+        print(self.rf_model.classification_report(self.y_test, y_pred))
         return y_pred
+    
+    def get_feature_imp_score(self):
+        # Get feature importances
+        return self.rf_model.feature_importances_
 
