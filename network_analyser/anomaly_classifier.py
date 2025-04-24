cbec19346ebe6cec7962aa94409f06646b134d6e
kyna <92019943+KynaChan@users.noreply.github.com>
2025-04-24 18:41:15 +0100

    Split trainer into two classes for IF and RF

--- network_analyser/anomaly_classifier.py
+++ network_analyser/anomaly_classifier.py
@@ -1,34 +1,39 @@
 
 
+import pandas as pd
+
 from sklearn.ensemble import RandomForestClassifier
 
 
 class AnomalyClassifier: 
 
-    def __init__(self, model: RandomForestClassifier, identified_anomalies):
+    def __init__(self, model: RandomForestClassifier, identified_anomalies_df: pd.DataFrame):
         self.rf_model = model
-        self.identified_anomalies = identified_anomalies
+        self.prep_data = identified_anomalies_df
 
-        self.rf_predictions = None
+    def run(self) -> pd.DataFrame:
+        try:
+            self.classify_anomalies()
+            return self.get_anomaly_port_df()
+
+        except Exception as e:
+            raise Exception(f"\n  [ERROR] An error occurred: {e}\n")
 
 
     def classify_anomalies(self):
         if self.rf_model is None:
             raise ValueError("[ERROR] Model not provided.")
-        
-        # Predict anomalies using the Isolation Forest model
-        self.rf_predictions = self.rf_model.predict(self.identified_anomalies)
-        
-        return self.rf_predictions
-    
-    def get_anomaly_ports(self):
+        self.prep_data['prediction'] = self.rf_model.predict(self.prep_data)
+
+
+    def get_anomaly_port_df(self):
         if self.rf_predictions is None:
-            raise ValueError("[ERROR] No predictions made yet.")
+            raise ValueError("[ERROR] No predictions available. Run classify_anomalies() first.")
         
-        # Get the indices of the anomalies
-        anomaly_indices = [i for i, x in enumerate(self.rf_predictions) if x == -1]
-        
-        # Get the corresponding ports
-        anomaly_ports = [self.identified_anomalies[i] for i in anomaly_indices]
-        
+        # Create a DataFrame with the predictions and ports
+        anomaly_df = pd.DataFrame({
+            "predictions": self.prep_data['prediction']==1,
+            "ports": self.prep_data['Destination Port']
+        })
+        anomaly_ports = anomaly_df[anomaly_df['predictions'] == 1]['anomaly_ports'].unique()
         return anomaly_ports
