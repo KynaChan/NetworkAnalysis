5c8f2cf5093bf318934a1f6094f031b683b0007c
kyna <92019943+KynaChan@users.noreply.github.com>
2025-04-26 10:46:20 +0100

    Edit file path

--- network_analyser/network_sniffer.py
+++ network_analyser/network_sniffer.py
@@ -15,13 +15,14 @@ class NetworkSniffer:
         self.interface = interface
         self.count = count
 
-        self.pcap_file_path = "sniffed_traffic.pcap"
+        self.pcap_file_path = "traffic_data/captured_traffic.pcap"
 
         self.output_df: pd.DataFrame | None = None
 
     def run(self) -> pd.DataFrame:
         try:
             self.capture_traffic()
+            self.rewrite_pcap()
             self.transform_traffic_data()
 
             return self.output_df
@@ -43,7 +44,12 @@ class NetworkSniffer:
         
         print(f"\n  [SUCCESS] Traffic captured and saved to {self.pcap_file_path}. (Packet count: {self.count})\n")
         
-        
+    # rewrite pcap file to add ethernet layer
+    def rewrite_pcap(self):
+        # tcprewrite --dlt=enet --infile="training_data/pcap/file_name.pcap" --outfile="training_data/pcap/file_name_eth.pcap"
+        # subprocess.run(["tcprewrite", "--dlt=enet", "--infile=" + self.pcap_file_path, "--outfile=" + self.pcap_file_path])
+        pass
+
 
     # Transform raw pcap to csv using CICFlowMeter
     def transform_traffic_data(self, output_file="transformed_data.csv"): 
