b4e9e236737092ed63b49071ee7d7870e2e4dfad
kyna <92019943+KynaChan@users.noreply.github.com>
2025-04-27 23:23:59 +0100

    Remove redundant trycatch

--- network_analyser/model_io.py
+++ network_analyser/model_io.py
@@ -3,29 +3,13 @@ import joblib
 
 class ModelIO:
     # Save the model to the specified file path
-    def save_model(
-        self, model, file_path
-    ):  # the TryCatch should i do it here or in main
-        try:
-            joblib.dump(model, file_path)
-            print(f"\n  [SUCCESS] Model saved to {file_path}\n")
-        except FileNotFoundError:
-            print(f"\n  [ERROR] File not found: {file_path}\n")
-        except Exception as e:
-            print(f"\n  [ERROR] An error occurred while saving the model: {e}\n")
+    def save_model(self, model, file_path):  # the TryCatch should i do it here or in main
+        joblib.dump(model, file_path)
+        print(f"\n  [SUCCESS] Model saved to {file_path}\n")
 
     # Load the model from the specified file path
     def load_model(self, file_path):
         model = None
-        try:
-            model = joblib.load(file_path)
-            print(f"\n  [SUCCESS] Model loaded from {file_path}\n")
-
-        except FileNotFoundError:
-            print(f"\n  [ERROR] File not found: {file_path}\n")
-
-        except Exception as e:
-            print(f"\n  [ERROR] An error occurred while loading the model: {e}\n")
-
-        # Return the loaded model
+        model = joblib.load(file_path)
+        print(f"\n  [SUCCESS] Model loaded from {file_path}\n")
         return model
